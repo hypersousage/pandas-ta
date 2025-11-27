b4698107e74ef7e72d2d3aac3440e4c3b68772c0
hypersousage <46888754+hypersousage@users.noreply.github.com>
2025-11-27 12:02:50 +0300

    Up version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "pandas_ta.volatility",
         "pandas_ta.volume"
     ],
-    version=".".join(("0", "4", "20b")),
+    version=".".join(("0", "4", "21b")),
     description=long_description,
     long_description=long_description,
     author="Kevin Johnson",
